04107df060099c49579a60e8e7dc721243620c9f
Lucas Ruiz <deth_lucas@hotmail.com>
2026-06-17 16:55:33 -0300

    tech challenge 02

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='segundo projeto de datascience da pós tech fiap',
+    author='Lucas Ruiz',
+    license='',
+)
