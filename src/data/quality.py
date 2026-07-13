4fc495dfaf85151edaa325f036c7cd46fba68575
Cecilia Fujita Abrahao <ceciliafa@192.168.0.166>
2026-07-12 22:11:42 -0300

    camada gold e tratamento no quality de campos int

--- src/data/quality.py
+++ src/data/quality.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 import pandas as pd
+import numpy as np
 
+# -----------------------------------------------
+# Colunas que devem ser convertidas para inteiro
+# -----------------------------------------------
 COLUNAS_INT = [
     "NU_ANO_AVALIACAO",
     "CO_UF",
@@ -10,71 +14,36 @@ COLUNAS_INT = [
     "TP_DEPENDENCIA",
     "CO_MUNICIPIO",
     "IN_PRESENCA_LP",
+    "IN_PREENCHIMENTO_LP",
+    "CO_CADERNO_LP",
+    "IN_ALFABETIZADO",
     "ID_TIPO_REDE",
     "ANO",
     "CO_BLOCO",
     "NU_POSICAO",
     "CO_ITEM",
-    "TP_SERIE",
-    "TP_DISCIPLINA"
-
+    "TP_DISCIPLINA",
+    "TP_RESPOSTA_ITEM",
+    "TP_MODELO_TRI",
+    "NU_PARAM_A",
+    "NU_PARAM_B",
+    "NU_PARAM_C",
+    "NU_PARAM_B1",
+    "NU_PARAM_B2",
+    "NU_PARAM_B3",
+    "NU_PARAM_B4",
+    "IN_ITEM_COMUM",
+    "CO_BLOCO_1",
+    "CO_BLOCO_2",
+    "CO_BLOCO_3",
+    "CO_BLOCO_4"
 ]
 
 
-def gerar_relatorio_data_quality(
-    bronze: pd.DataFrame,
-    silver: pd.DataFrame,
-    ano: int,
-    tabela: str,
-    camada: str = "silver"
-) -> pd.DataFrame:
-    """
-    Gera um relatório de Data Quality da execução.
-    """
 
-    relatorio = {
-
-        "DT_EXECUCAO": datetime.now(),
-
-        "CAMADA": camada,
-
-        "ANO": ano,
-
-        "TABELA": tabela,
-
-        "LINHAS_BRONZE": len(bronze),
-
-        "LINHAS_SILVER": len(silver),
-
-        "COLUNAS": len(silver.columns),
-
-        "DUPLICADOS_BRONZE": int(bronze.duplicated().sum()),
-
-        "DUPLICADOS_SILVER": int(silver.duplicated().sum()),
-
-        "LINHAS_VAZIAS_BRONZE": int(
-            bronze.isna().all(axis=1).sum()
-        ),
-
-        "LINHAS_VAZIAS_SILVER": int(
-            silver.isna().all(axis=1).sum()
-        ),
-
-        "NULOS_BRONZE": int(
-            bronze.isna().sum().sum()
-        ),
-
-        "NULOS_SILVER": int(
-            silver.isna().sum().sum()
-        ),
-
-        "STATUS": "OK"
-    }
-
-    return pd.DataFrame([relatorio])
-import pandas as pd
-import numpy as np
-
+# -----------------------------------------------
+# Funções de Data Quality
+# -----------------------------------------------
 
 def remover_linhas_vazias(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -137,35 +106,29 @@ def padronizar_textos(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def converter_tipos(
-    df: pd.DataFrame,
-    dicionario: pd.DataFrame
-) -> pd.DataFrame:
+def converter_colunas_int(df: pd.DataFrame, colunas: list[str]) -> pd.DataFrame:
     """
-    Converte tipos automaticamente.
-
-    Atualmente converte apenas números.
-    Pode ser expandido futuramente usando o dicionário.
+    Converte as colunas informadas para inteiro (Int64),
+    ignorando colunas inexistentes.
     """
 
-    for coluna in df.columns:
+    for coluna in colunas:
 
-        # ignora colunas inexistentes no dicionário
-        if coluna not in dicionario.index:
-            continue
+        if coluna in df.columns:
 
-        serie = df[coluna]
-
-        # tenta converter para inteiro
-        convertido = pd.to_numeric(
-            serie,
-            errors="ignore"
-        )
-
-        df[coluna] = convertido
+            df[coluna] = (
+                pd.to_numeric(
+                    df[coluna],
+                    errors="coerce"
+                )
+                .astype("Int64")
+            )
 
     return df
 
+# -----------------------------------------------
+# Função principal de Data Quality
+# -----------------------------------------------   
 
 def aplicar_data_quality(
     df: pd.DataFrame,
@@ -193,10 +156,67 @@ def aplicar_data_quality(
 
     print(f"Textos padronizados : {df.select_dtypes(include='object').columns.size}")
 
- #   df = converter_tipos(df, dicionario)
+    df = converter_colunas_int(df, COLUNAS_INT)
 
- #   print(f"Tipos convertidos : {df.select_dtypes(include=np.number).columns.size}")
+    print(f"Colunas convertidas : {df.select_dtypes(include='Int64').columns.size}")
 
     print(f"Data Quality concluída.")
 
     return df
+
+
+#   -----------------------------------------------
+#   Relatório de Data Quality
+#   -----------------------------------------------
+
+def gerar_relatorio_data_quality(
+    bronze: pd.DataFrame,
+    silver: pd.DataFrame,
+    ano: int,
+    tabela: str,
+    camada: str = "silver"
+) -> pd.DataFrame:
+    """
+    Gera um relatório de Data Quality da execução.
+    """
+
+    relatorio = {
+
+        "DT_EXECUCAO": datetime.now(),
+
+        "CAMADA": camada,
+
+        "ANO": ano,
+
+        "TABELA": tabela,
+
+        "LINHAS_BRONZE": len(bronze),
+
+        "LINHAS_SILVER": len(silver),
+
+        "COLUNAS": len(silver.columns),
+
+        "DUPLICADOS_BRONZE": int(bronze.duplicated().sum()),
+
+        "DUPLICADOS_SILVER": int(silver.duplicated().sum()),
+
+        "LINHAS_VAZIAS_BRONZE": int(
+            bronze.isna().all(axis=1).sum()
+        ),
+
+        "LINHAS_VAZIAS_SILVER": int(
+            silver.isna().all(axis=1).sum()
+        ),
+
+        "NULOS_BRONZE": int(
+            bronze.isna().sum().sum()
+        ),
+
+        "NULOS_SILVER": int(
+            silver.isna().sum().sum()
+        ),
+
+        "STATUS": "OK"
+    }
+
+    return pd.DataFrame([relatorio])
